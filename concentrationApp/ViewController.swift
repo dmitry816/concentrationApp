80a8ba90eae0a8635fe48363d9d737a0b230d120
dmitry816 <dmineev816@gamil.com>
2021-05-23 18:40:41 +0300

    IBOutlet, IBAction, viewController

--- concentrationApp/ViewController.swift
+++ concentrationApp/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    var emojiChoices = ["💣", "🛡", "🔪", "⚰️", "🧿", "💈", "💊", "🔫"]
+    
+    @IBOutlet var cardButtons: [UIButton]!
+
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("choosen card not in set")
+        }
     }
 
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0, green: 0.5603182912, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+        }
+    }
+    
 
 }
 
